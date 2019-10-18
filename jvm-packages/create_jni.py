0458830f8cfe9e0ac0981c527bd1b041ce58927d
Liangcai Li <liangcail@nvidia.com>
2019-10-18 18:07:09 +0800

    Add rmm library to dependence list
    and pack it into xgboost4j jar file.
    
    Becasue the rmm APIs used by XGBoost are no longer header files only,
    instead are moved into library (librmm.so).

--- jvm-packages/create_jni.py
+++ jvm-packages/create_jni.py
@@ -5,6 +5,7 @@ import os
 import shutil
 import subprocess
 import sys
+import re
 from contextlib import contextmanager
 
 
@@ -68,6 +69,13 @@ def normpath(path):
     else:
         return normalized
 
+def find_rmmlib(xgblib):
+    linkinfo = os.popen('ldd ' + xgblib).read()
+    ret = re.search(r'librmm\.so\s+=>\s+(.*librmm.*\.so)\s+.*', linkinfo)
+    if ret:
+        return ret.group(1)
+    else:
+        raise Exception("Can not find rmm library.")
 
 if __name__ == "__main__":
     if len(sys.argv) >= 2 and sys.argv[1].lower() == "cuda9.2":
@@ -116,6 +124,8 @@ if __name__ == "__main__":
     lib_path = "xgboost4j/src/main/resources/lib/" + cuda
     maybe_makedirs(lib_path)
     cp("../lib/" + library_name, lib_path)
+    rmmlib = find_rmmlib(os.path.join(lib_path, library_name))
+    cp(rmmlib, lib_path)
 
     print("copying pure-Python tracker")
     cp("../dmlc-core/tracker/dmlc_tracker/tracker.py",
